042ceb13fc7eca86c438f7276cb08e7a6450598b
Kenneth <kmarshall_pvamu@yahoo.com>
2021-05-26 11:14:22 -0500

    band api almost consumed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {  FormsModule } from '@angular/forms'; //FormControl,
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,8 @@ import { VenuecardComponent } from './venuecard/venuecard.component';
 import { UserBandListComponent } from './user-band-list/user-band-list.component';
 import { ProfilepageComponent } from './profilepage/profilepage.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NewsearchService } from './newsearch.service';
+import { ApibandlistComponent } from './apibandlist/apibandlist.component';
 
 
 @NgModule({
@@ -24,16 +27,18 @@ import { NavbarComponent } from './navbar/navbar.component';
     UserBandListComponent,
     ProfilepageComponent,
     NavbarComponent,
+    ApibandlistComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
     // FormControl //not sure if this is right one, need ot learn about it!
     //reactiveFormsModule...?
   ],
-  providers: [],
+  providers: [NewsearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
