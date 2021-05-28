572773ce9b4db834bc2b8a00d62c524a7466b690
petergrace1618 <petergrace1.618@gmail.com>
2021-05-28 09:15:38 -0700

    updated fontloginpage

--- src/app/frontloginpage/frontloginpage.component.ts
+++ src/app/frontloginpage/frontloginpage.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./frontloginpage.component.css']
 })
 export class FrontloginpageComponent implements OnInit {
-  title: string ="Welcome To The Land of Schlarb";
+  title: string ="Welcome to Schlarb Land";
+  appDescription: string = "Schlarb is a music app where users can save their favorite musicians and receive updates when they are going to be performing live at one of their selected venues. Users will have their own profile where they can update their bio, like other lists, see other lists, and save their favorite artists to a list and see where they are performing.";
 
   constructor() { }
 
