67a0e4e9c98b6f75ed36865e778a4816e253aa23
Kenneth <kmarshall_pvamu@yahoo.com>
2021-05-31 21:58:46 -0500

    beauty

--- src/app/band.ts
+++ src/app/band.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 export class Band{
-=======
-export interface Band{
->>>>>>> 6e584660f980bbeaab742ae34e984c15585eae89
     id?: number;
     name: string;
     url: string;
@@ -11,12 +7,4 @@ export interface Band{
     mbid: string; //?
     tracker_count: number; //?
     upcoming_event_count: number;
-<<<<<<< HEAD
-=======
-    //supporturl    support_url
-    //options
-    //these were nto in docs but show up - do I need to include them?
-    // options?: object; //object might need details...
-    // support_url?: string;
->>>>>>> 6e584660f980bbeaab742ae34e984c15585eae89
 }
--- src/app/newsearch.service.ts
+++ src/app/newsearch.service.ts
@@ -14,6 +14,10 @@ import { catchError, retry } from 'rxjs/operators';
 })
 export class NewsearchService {
 
+  private artistUrl: string = "https://rest.bandsintown.com/artists/"; //url for artist search
+  private ak:string = "/?app_id=de960fdbd41b94a4ccd7234c7da4f8ae";  //put on end of api requests
+    
+    
   private urlCrud: string;
    private urlAdd: string;
   constructor(private http: HttpClient) {
@@ -31,9 +35,9 @@ export class NewsearchService {
     
   }
 
-  getBandsFromApi(): Observable<Band[]> {
+  getBandFromApi(query): Observable<Band> {
     // console.log("api: " + );
-    return this.http.get<Band[]>(this.urlCrud); 
+    return this.http.get<Band>(this.artistUrl+query+this.ak); 
     //replace this with the real api 
   }
 
@@ -43,94 +47,3 @@ export class NewsearchService {
 
   }
 }
-  // getBandsFromApi(query): Observable<Band[]> {
-  //   console.log("api: " + query);
-  //   return this.http.get<Band[]>("https://rest.bandsintown.com/artists/${query}/events/?app_id=de960fdbd41b94a4ccd7234c7da4f8a"); 
-  //   //replace this with the real api 
-  // }
-  //We want this to work and implement the string literal of the the query
-
-
-
-// (method) HttpClient.get(url: string, options: {
-//   headers?: HttpHeaders | {
-//       [header: string]: string | string[];
-//   };
-//   observe?: "body";
-//   params?: HttpParams | {
-//       [param: string]: string | string[];
-//   };
-//   reportProgress?: boolean;
-//   responseType: "arraybuffer";
-//   withCredentials?: boolean;
-// }): Observable<...> (+14 overloads)
-
-
-
-
-
-
-
-
-
-
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class NewsearchService {
-//   //newsearch should get bands or venues from external api
-
-//   getBands(): Observable<Band[]> {
-//     const bands = of(
-//       [
-//         {
-//         id: 5,
-//         name: "test",
-//         url: "string",
-//         image_url: "string",
-//         facebook_page_url: "string",
-//         mbid: "string", 
-//         tracker_count: 2, 
-//         upcoming_event_count: 1,
-//       },
-//       {
-//         id: 4,
-//         name: "test",
-//         url: "string",
-//         image_url: "string",
-//         facebook_page_url: "string",
-//         mbid: "string", 
-//         tracker_count: 3, 
-//         upcoming_event_count: 1
-//       }
-//       ]
-//     );
-//     return bands;
-//     //this will need to be replaces with real api data
-//   }
-//   getBandsFromApi(query) {
-//     console.log("api: " + query);
-//     return this.httpClient.get<Band>("https://pokeapi.co/api/v2/pokemon/123"); //replace this with the real api 
-
-
-//   }
-//   getVenues(): Observable<Venue[]> {
-//     const venues = of(
-//       [{
-//         name: "string",
-//         latitude: "string",
-//         longitude: "string",
-//         city: "string",
-//         region: "string",
-//         country: "string"
-//       }
-//       ]
-//     );
-//     return venues;
-//   }
-
-//   constructor(protected httpClient: HttpClient) { }
-//   // constructor(protected httpClient: HttpClient, @Optional()@Inject(BASE_PATH) basePath: string, @Optional() configuration: Configuration) {
-// }
-
