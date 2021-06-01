7c173a94a490946cd20650420c12378d92f2a084
Beekenstein <bkelly0905@gmail.com>
2021-05-31 21:55:07 -0400

    testing

--- src/app/apibandlist/apibandlist.component.spec.ts
+++ src/app/apibandlist/apibandlist.component.spec.ts
@@ -1,13 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {HttpTestingController} from "@angular/common/http/testing";
 
 import { ApibandlistComponent } from './apibandlist.component';
 
 describe('ApibandlistComponent', () => {
   let component: ApibandlistComponent;
   let fixture: ComponentFixture<ApibandlistComponent>;
+  let httpMock: HttpTestingController; // It is basically a mock version of the HTTPClient used for testing purposes. 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpTestingController],
+      providers:[ApibandlistComponent],
       declarations: [ ApibandlistComponent ]
     })
     .compileComponents();
